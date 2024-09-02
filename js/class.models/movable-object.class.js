77a6af78994a17e3d73476ef291c43690595fb50
Metafeller <savasboas@gmail.com>
2024-09-02 16:22:42 +0200

    add constructor for character and enemies

--- js/class.models/movable-object.class.js
+++ js/class.models/movable-object.class.js
@@ -4,6 +4,13 @@ class MovableObject {
     img;
 
 
+    // loadImage('img/test.png');
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
+        this.img.src = path;
+    }
+
+
     moveRight() {
     console.log('Moving right');
     }
