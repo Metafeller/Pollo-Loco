8df9408505b39d7ab2e2edd0f28c217f297c5303
Metafeller <savasboas@gmail.com>
2024-09-03 18:36:19 +0200

    add loadImages([ ])
    Alle Bilder vom Charakter eingefügt für die Animation, die danach kommt...

--- js/class.models/movable-object.class.js
+++ js/class.models/movable-object.class.js
@@ -1,9 +1,11 @@
 class MovableObject {
     x = 120;
     y = 280;
+    img;
     height = 150;
     width = 100;
-    img;
+
+    imageCache = [];
 
 
     // loadImage('img/test.png');
@@ -12,12 +14,23 @@ class MovableObject {
         this.img.src = path;
     }
 
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+    }
 
     moveRight() {
     console.log('Moving right');
     }
 
-    moveLeft(){
+    moveLeft() {
     console.log('Moving left');
         
     }
