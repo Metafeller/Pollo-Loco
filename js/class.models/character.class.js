0901babc50a4d83d83df3e761b32fa8af19ebfa1
Metafeller <savasboas@gmail.com>
2024-09-03 20:55:35 +0200

    add walk animate left and right (bug)
    Der Charakter kann jetzt von rechts nach links und von rechts nach links laufen (Beta). Hat noch ein Bug, der Charakter bewegt sich nicht...

--- js/class.models/character.class.js
+++ js/class.models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
 
     height = 280;
     y = 160;
+    speed = 10;
     IMAGES_WALKING = [
         '/img/2_character_pepe/2_walk/W-21.png',
         '/img/2_character_pepe/2_walk/W-22.png',
@@ -10,6 +11,7 @@ class Character extends MovableObject {
         '/img/2_character_pepe/2_walk/W-25.png',
         '/img/2_character_pepe/2_walk/W-26.png'
     ];
+    world;
 
     constructor() {
         super().loadImage('/img/2_character_pepe/2_walk/W-21.png');
@@ -19,13 +21,31 @@ class Character extends MovableObject {
     }
 
     animate() {
+
         setInterval(() => {
-        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % "7 geteilt durch 6 ist Eins" 6; => (1, Rest 1)
-        // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0...
-        let path = this. IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-        }, 300);
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+
+        setInterval(() => {
+        
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.x += this.speed;
+
+                // walk animation
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % "7 geteilt durch 6 ist Eins" 6; => (1, Rest 1)
+                // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0...
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 50);
     }
 
     jump() {
