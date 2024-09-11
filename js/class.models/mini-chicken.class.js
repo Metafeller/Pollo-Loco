d36f65b33ef534a89a339a2bb5db6e0e4acb3cdd
Metafeller <savasboas@gmail.com>
2024-09-11 16:37:52 +0200

    Chicks die when they jump on it
    Die Küken sterben beim draufspringen...

--- js/class.models/mini-chicken.class.js
+++ js/class.models/mini-chicken.class.js
@@ -8,15 +8,22 @@ class MiniChicken extends MovableObject {
         '/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
 
+    IMAGES_DEAD = [
+        '/img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
+
 
     constructor() {
-        super().loadImage('/img/3_enemies_chicken/chicken_small/1_walk/1_w.png')
+        super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-
-        this.x = 5000 + Math.random() * 500;
-        this.speed = 0.2 + Math.random() * 0.3; // Mini-Chicken haben eine etwas schnellere Geschwindigkeit
-
+        this.loadImages(this.IMAGES_DEAD);
+        this.x = 3000 + Math.random() * 500;  // Zufällige Position
+        this.speed = 0.15 + Math.random() * 0.5;
         this.animate();
+    }
+
+    die() {
+        this.loadImage(this.IMAGES_DEAD[0]);  // Lässt das Huhn sterben
     }
 
 
